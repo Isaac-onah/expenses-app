5e74b95a01b8ec5393c1dd6a3a5b88bcf533eb8e
Isaac Onah <iumeha@yahoo.com>
2023-01-26 11:51:02 +0100

    just getting started

---
+++ lib/icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+    Map<String, IconData> icons ={
+        'Auto and Transport' : Icons.train,
+      'Sport':Icons.sports_soccer,
+      'Food and Drinks': Icons.local_pizza,
+      'Entertainment' : Icons.movie,
+      'Education': Icons.school,
+      'Other':Icons.miscellaneous_services,
+    };
---
+++ lib/models/ex_category.dart
@@ -0,0 +1,42 @@
+
+import 'package:first/icons.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseCategory{
+  final String title;
+  int entries =0; //how many expenses are in this category. it will change overtime
+  double totalAmount = 0.0; //total amount of expenses in this category
+  final IconData icon; //we will define some constant icons
+
+  //constructor
+  ExpenseCategory({
+    required this.title,
+    required this.entries,
+    required this.totalAmount,
+    required this.icon,
+});
+
+  //we need a method to convert this 'model' to a 'map'
+//so that we can insert it into a database
+
+Map<String, dynamic> toMap() =>
+    {
+      'title': title,
+      'entries': entries,
+      'totalAmount': totalAmount.toString(),
+      //our database wont be able to store double values so we convert to a string
+      //not gonna store icons in database. that is too much work.
+    };
+
+
+//when we retrive the data from the database it will be a 'map'
+//for our app to understand the data, we need to convert it back to expenses category
+
+factory ExpenseCategory.fromString(Map<String, dynamic> value)=>ExpenseCategory(
+    title: value['title'],
+    entries: value['entries'],
+    totalAmount: double.parse(value['totalAmount']),
+    //it will search the 'icons' map and find the value related to the title.
+    icon: icons[value['title']]!);
+
+}

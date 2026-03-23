40ef725cdd3ada92890a518e17ed10738bf5888f
Aiyzihk <isaaconah6@gmail.com>
2026-03-23 13:54:03 +0100

    Remove comment about Center widget
    Removed comment explaining the Center widget.

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('test'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
 
